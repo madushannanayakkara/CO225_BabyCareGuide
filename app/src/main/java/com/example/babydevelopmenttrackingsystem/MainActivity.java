649cdc3ef46b3b900a2bb19e2aa7814efe226674
e18264 <e18264@eng.pdn.ac.lk>
2022-02-06 21:21:08 +0530

    starting the main branch

---
+++ app/src/main/java/com/example/babydevelopmenttrackingsystem/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.babydevelopmenttrackingsystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Button btn = findViewById(R.id.btnRegister);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent =  new Intent(MainActivity.this, RegisterForm.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
