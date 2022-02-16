dcfbb37e164c39e0e48a7949ad43887c94758365
e18264 <e18264@eng.pdn.ac.lk>
2022-02-17 01:46:38 +0530

    delete -([200~RegisterForm.java

--- app/src/main/java/com/example/babydevelopmenttrackingsystem/MainActivity.java
+++ app/src/main/java/com/example/babydevelopmenttrackingsystem/MainActivity.java
@@ -1,28 +1,90 @@
 package com.example.babydevelopmenttrackingsystem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private BottomNavigationView bottomNav;
+    private Button btnDetails, btnVaccination, btnBMI, btnSettings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btnDetails = findViewById(R.id.btnDetails);
+        btnVaccination = findViewById(R.id.btnVaccination);
+        btnBMI = findViewById(R.id.btnBmi);
+        btnSettings = findViewById(R.id.btnSettings);
 
-        Button btn = findViewById(R.id.btnRegister);
-        btn.setOnClickListener(new View.OnClickListener() {
+        btnDetails.setOnClickListener(this);
+        btnVaccination.setOnClickListener(this);
+        btnBMI.setOnClickListener(this);
+        btnSettings.setOnClickListener(this);
+
+        bottomNav = findViewById(R.id.bottom_navigator);
+        bottomNav.setSelectedItemId(R.id.nav_home);
+
+        bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void onClick(View view) {
-                Intent intent =  new Intent(MainActivity.this, RegisterForm.class);
-                startActivity(intent);
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.nav_home:
+                        return true;
+                    case R.id.nav_details:
+                        startActivity(new Intent(getApplicationContext(), Details.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                    case R.id.nav_vaccination:
+                        startActivity(new Intent(getApplicationContext(), Vaccination.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                    case R.id.nav_bmi:
+                        startActivity(new Intent(getApplicationContext(), bmi.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                    case R.id.nav_settings:
+                        startActivity(new Intent(getApplicationContext(), settings.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                }
+
+                return false;
             }
         });
 
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btnDetails:
+                startActivity(new Intent(getApplicationContext(), Details.class));
+                overridePendingTransition(0, 0);
+                break;
+            case R.id.btnVaccination:
+                startActivity(new Intent(getApplicationContext(), Vaccination.class));
+                overridePendingTransition(0, 0);
+                break;
+            case R.id.btnBmi:
+                startActivity(new Intent(getApplicationContext(), bmi.class));
+                overridePendingTransition(0, 0);
+                break;
+            case R.id.btnSettings:
+                startActivity(new Intent(getApplicationContext(), settings.class));
+                overridePendingTransition(0, 0);
+                break;
+            default:
+                break;
+        }
     }
 }
