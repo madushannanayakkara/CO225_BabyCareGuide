915bffd8f53247eb59d2952a01962a9081d2e300
muthuni-dealwis <kkmdealwis@gmail.com>
2022-05-19 16:33:24 +0530

    Added vaccination data to database

--- app/src/main/java/com/example/babydevelopmenttrackingsystem/DatabaseH.java
+++ app/src/main/java/com/example/babydevelopmenttrackingsystem/DatabaseH.java
@@ -2,6 +2,8 @@ package com.example.babydevelopmenttrackingsystem;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.widget.Toast;
@@ -15,6 +17,10 @@ class DatabaseH extends SQLiteOpenHelper {
     private static final int DATABASE_VERSION = 1;
 
     private static final String TABLE_NAME = "baby_Details";
+    private static final String LASTSAVEDID = "last_saved_id";
+    private static final String TABLE3 = "vaccines";
+    private static final String TABLE4 = "notify";
+
     private static final String COLUMN_ID = "baby_id";
     private static final String COLUMN_FNAME = "first_name";
     private static final String COLUMN_LNAME = "last_name";
@@ -22,6 +28,20 @@ class DatabaseH extends SQLiteOpenHelper {
 
     private static final String COLUMN_WEIGHT = "current_weight";
     private static final String COLUMN_HEIGHT = "current_height";
+
+
+    //Column names of the table3
+    private static final String ID = "id";
+    //private static final String AGE = "age";
+    private static final String VACCINE = "vaccine";
+    //private static final String REMARKS = "remarks";
+    private static final String DONE = "done";
+    private static final String USERID = "userId";
+
+    //Column names for table4
+    private static final String TIMEPERIOD = "timePeriod";
+
+
 
     public DatabaseH(@Nullable Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -39,15 +59,35 @@ class DatabaseH extends SQLiteOpenHelper {
                 COLUMN_WEIGHT + " INTEGER, " +
                 COLUMN_HEIGHT + " INTEGER);";
         db.execSQL(query);
+
+        String query2 = "CREATE TABLE " + LASTSAVEDID +
+                " (" + COLUMN_ID + " INTEGER);";
+        db.execSQL(query2);
+
+        //Query to add vaccination data
+        String query3 = "CREATE TABLE " + TABLE3 + " ("
+                +ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                +USERID + " Integer references " + TABLE_NAME + "(" + COLUMN_ID + "),"
+                +VACCINE + " TEXT, "
+                +DONE + " TEXT);" ;
+
+        db.execSQL(query3);
+
+
+        //Query to add notification settings data
+        String query4 = "CREATE TABLE " + TABLE4 +
+                " (" + TIMEPERIOD + " TEXT);";
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int i, int i1) {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " + LASTSAVEDID);
+        db.execSQL("DROP TABLE IF EXISTS "+ TABLE3);
         onCreate(db);
     }
 
-    void addBaby(String fname, String lname, String bdate, int weight, int height){
+    public void addBaby(String fname, String lname, String bdate, int weight, int height){
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues cv = new ContentValues();
 
@@ -65,4 +105,106 @@ class DatabaseH extends SQLiteOpenHelper {
             Toast.makeText(context, "Added successfully !!", Toast.LENGTH_SHORT).show();
         }
     }
+
+    public void addVaccineData(String name, String done, int vaccined/*int userId*/){
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+
+
+        contentValues.put(VACCINE, name);
+        //contentValues.put(USERID, userId);
+        contentValues.put(DONE, done);
+
+        /* Save to the table*/
+
+        if(vaccined == 0) {
+            long result = db.insert(TABLE3, null, contentValues);
+
+            if (result == -1) {
+                Toast.makeText(context, "Failed!", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(context, "Added successfully !!", Toast.LENGTH_SHORT).show();
+            }
+        }
+        else{
+            Toast.makeText(context, "This vaccine is already given:)", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    Cursor readAccounts(){
+        String query = "SELECT " + COLUMN_FNAME + ", " + COLUMN_LNAME + " FROM " + TABLE_NAME;
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = null;
+        if(db != null){
+            cursor = db.rawQuery(query, null);
+        }
+        return cursor;
+    }
+
+    int readLastSavedID(){
+        String query = "SELECT * FROM " + LASTSAVEDID;
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        if(db != null){
+            Cursor cursor = db.rawQuery(query, null);
+            if(cursor.getCount() == 0){
+                return -1;
+            } else {
+                return cursor.getInt(0);
+            }
+        }
+        return -2;
+    }
+
+    void updateLastSavedID(int id){
+        int last_saved_id = readLastSavedID();
+        String query = null;
+
+        if (last_saved_id > 0){
+            query = "UPDATE " + LASTSAVEDID + " SET " + COLUMN_ID + " = " + id + " WHERE " + COLUMN_ID + " = " + readLastSavedID();
+        }else if(last_saved_id == -1){
+            query = "INSERT INTO " + LASTSAVEDID + " VALUES (" + id + ");";
+        }
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.rawQuery(query, null);
+
+    }
+
+    public int findID(String Fname, String Lname){
+        String query = "SELECT " + COLUMN_ID + " FROM " + TABLE_NAME + " WHERE " + COLUMN_FNAME + " = " + Fname + " && " + COLUMN_LNAME + " = " + Lname;
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        if(db != null){
+            Cursor cursor = db.rawQuery(query, null);
+            if(cursor.getCount() == 0){
+                return -1;
+            } else {
+                return cursor.getInt(0);
+            }
+        }
+        return -2;
+    }
+
+    public String getDate(int ID){
+        String query = "SELECT " + COLUMN_BIRTHDATE + " FROM " + TABLE_NAME + " WHERE " + COLUMN_ID + " = " + ID ;
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        if(db != null){
+            Cursor cursor = db.rawQuery(query, null);
+            if(cursor.getCount() == 0){
+                return null;
+            } else {
+                return cursor.getString(0);
+            }
+        }
+        return null;
+
+    }
+
+
+
 }
--- app/src/main/java/com/example/babydevelopmenttrackingsystem/Vaccination.java
+++ app/src/main/java/com/example/babydevelopmenttrackingsystem/Vaccination.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Choreographer;
@@ -21,11 +22,15 @@ import java.util.Date;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class Vaccination extends AppCompatActivity {
+public class Vaccination extends AppCompatActivity  {
 
     private CheckBox cb1, cb2, cb3, cb4, cb5, cb6, cb7, cb8, cb9;
     private Button calcButton;
-    private TextView text;
+    private TextView text, vac;
+    private DatabaseH databaseH;
+    private int[] count = {0,0,0,0,0,0,0,0,0};
+    private static int vaccined = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +39,9 @@ public class Vaccination extends AppCompatActivity {
 
         BottomNavigationView bottomNav = findViewById(R.id.bottom_navigator);
         bottomNav.setSelectedItemId(R.id.nav_vaccination);
+
+        //context = this;
+        databaseH = new DatabaseH(Vaccination.this);
 
         bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
@@ -63,6 +71,7 @@ public class Vaccination extends AppCompatActivity {
             }
         });
 
+        //Variables used to calculate the next vaccination date
         cb1 = findViewById(R.id.cb1);
         cb2 = findViewById(R.id.cb2);
         cb3 = findViewById(R.id.cb3);
@@ -75,50 +84,112 @@ public class Vaccination extends AppCompatActivity {
         calcButton = findViewById(R.id.calcButton);
         text = findViewById(R.id.date);
 
+        /*int id = databaseH.readLastSavedID();
+        String d1 = databaseH.getDate(id);*/
+
+        //Variables to add data to the database
+
+
         String d1 = "2017-01-29";
+
 
         calcButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 if(cb9.isChecked()){
+
+                    vac = findViewById(R.id.r102);
+
                     String strDate = setDate(d1, 2, 60);
                     text.setText(strDate);
+
+                    vaccined = count[0];
+                    count[0]++;
                 }
                 else if(cb8.isChecked()){
+
+                    vac = findViewById(R.id.r92);
+
                     String strDate = setDate(d1, 2, 36);
                     text.setText(strDate);
+
+                    vaccined = count[1];
+                    count[1]++;
                 }
                 else if(cb7.isChecked()){
+
+                    vac = findViewById(R.id.r82);
+
                     String strDate = setDate(d1, 2, 18);
                     text.setText(strDate);
+
+                    vaccined = count[2];
+                    count[2]++;
                 }
                 else if(cb6.isChecked()){
+
+                    vac = findViewById(R.id.r72);
+
                     String strDate = setDate(d1, 2, 12);
                     text.setText(strDate);
+
+                    vaccined = count[3];
+                    count[3]++;
                 }
                 else if(cb5.isChecked()){
+
+                    vac = findViewById(R.id.r62);
+
                     String strDate = setDate(d1, 2, 9);
                     text.setText(strDate);
+
+                    vaccined = count[4];
+                    count[4]++;
                 }
                 else if(cb4.isChecked()){
+
+                    vac = findViewById(R.id.r52);
+
                     String strDate = setDate(d1, 2, 6);
                     text.setText(strDate);
+
+                    vaccined = count[5];
+                    count[5]++;
                 }
                 else if(cb3.isChecked()){
-                   String strDate = setDate(d1, 2, 4);
+
+                    vac = findViewById(R.id.r42);
+
+                    String strDate = setDate(d1, 2, 4);
                     text.setText(strDate);
+
+                    vaccined = count[6];
+                    count[6]++;
                 }
                 else if(cb2.isChecked()){
+
+                    vac = findViewById(R.id.r32);
+
                     String strDate = setDate(d1, 2, 2);
                     text.setText(strDate);
+
+                    vaccined = count[7];
+                    count[7]++;
                 }
                 else if(cb1.isChecked()){
-                   String strDate = setDate(d1, 1, 14);
+
+                    vac = findViewById(R.id.r22);
+
+                    String strDate = setDate(d1, 1, 14);
                     text.setText(strDate);
+
+                    vaccined = count[8];
+                    count[8]++;
 
                 }
 
-
+                databaseH.addVaccineData(vac.getText().toString().trim(),"done",vaccined);
 
             }
         });
@@ -157,13 +228,9 @@ public class Vaccination extends AppCompatActivity {
         }
 
         //Date after adding the days to the given date
-
-
         return updated;
     }
 
-
-}
 
     @Override
     public void onBackPressed() {
